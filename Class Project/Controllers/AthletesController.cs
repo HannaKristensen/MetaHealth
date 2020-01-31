9767969e8e0cd59aceb8edcdca4f132ee48d800c
HannaKristensen <hkristensen15@wou.edu>
2020-01-30 16:24:03 -0800

    Added racetimes to athestes details page

--- Class Project/Controllers/AthletesController.cs
+++ Class Project/Controllers/AthletesController.cs
@@ -44,6 +44,7 @@ namespace Class_Project.Views
         public ActionResult Create()
         {
             ViewBag.TeamID = new SelectList(db.Teams, "TeamID", "Name");
+            ViewBag.Gender = new SelectList(db.Athletes, "Gender", "Gender");
             return View();
         }
 
@@ -62,6 +63,7 @@ namespace Class_Project.Views
             }
 
             ViewBag.TeamID = new SelectList(db.Teams, "TeamID", "Name", athlete.TeamID);
+            ViewBag.GenderID = new SelectList(db.Athletes, "Gender", "Gender", athlete.Gender);
             return View(athlete);
         }
 
