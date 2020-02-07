d91aae6ae141ee292dbc6e07dc23408cff78cfca
HannaKristensen <hkristensen15@wou.edu>
2020-02-06 17:42:11 -0800

    Testing Azure Stuff

---
+++ MetaHealth/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MetaHealth.Startup))]
+namespace MetaHealth
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
