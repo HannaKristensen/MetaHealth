4ca5201a6272234673c269d23b58217198e138c6
HannaKristensen <hkristensen15@wou.edu>
2020-02-12 15:17:44 -0800

    Added seed data

--- MetaHealth/App_Data/seed.sql
+++ MetaHealth/App_Data/seed.sql
@@ -1,7 +1,12 @@
 ﻿INSERT INTO [dbo].[PreLevelList] VALUES
-(1,'Take Medication'),
-(1,'Drink water'),
 (1, 'Get out of bed'),
+(1, 'Drink a glass of water'),
+(1, 'Eat a meal'),
+(2, 'Take a shower'),
+(2, 'Talk to someone'),
 (2, 'Brush Teeth'),
-(2, 'Eat some food'),
-(2, 'Shower')
+(2, 'Put on Deodorant'),
+(3, 'Make your bed'),
+(3, 'Talk to someone face to face'),
+(3, 'Go on a walk'),
+(3, 'Pick up room')
