853bb4bb7b0a6caf9c6b21af69d41d69b0557182
Jasmit Singh <jsingh18@wou.edu>
2020-02-19 19:13:41 -0800

    attempting calender new way

---
+++ MetaHealth/Controllers/CalendarController.cs
@@ -0,0 +1,51 @@
+﻿/*
+Copyright 2015 Google Inc
+
+Licensed under the Apache License, Version 2.0(the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Auth.OAuth2.Flows;
+using Google.Apis.Auth.OAuth2.Responses;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using MetaHealth.Models;
+
+namespace GoogleCalendarApi.Controllers
+{
+    [Authorize]
+    public class CalendarController : Controller
+    {
+
+        // GET: /Calendar/UpcomingEvents
+        public ActionResult UpcomingEvents()
+        {
+            CalendarEventGroup viewModel = new CalendarEventGroup();
+
+            return View(viewModel);
+
+        }
+
+
+    }
+}
