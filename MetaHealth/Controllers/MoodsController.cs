d63545552c67910b2bd7b71bfb6016a9f152e5a7
Bmiller <millerbrittany01@gmail.com>
2020-02-29 20:18:35 -0800

    removed edit/delete buttons

---
+++ MetaHealth/Controllers/MoodsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MetaHealth;
+
+namespace MetaHealth.Controllers
+{
+    public class MoodsController : Controller
+    {
+        private HelpAlongDB db = new HelpAlongDB();
+
+        // GET: Moods
+        public ActionResult Index()
+        {
+            var moods = db.Moods.Include(m => m.MoodsInBetween);
+            return View(moods.ToList());
+        }
+
+        // GET: Moods/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Mood mood = db.Moods.Find(id);
+            if (mood == null)
+            {
+                return HttpNotFound();
+            }
+            return View(mood);
+        }
+
+        // GET: Moods/Create
+        public ActionResult Create()
+        {
+            ViewBag.FK_MoodsInBetween = new SelectList(db.MoodsInBetweens, "PK", "FK_UserTable");
+            return View();
+        }
+
+        // POST: Moods/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PK,FK_MoodsInBetween,MoodNum,Date")] Mood mood)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Moods.Add(mood);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.FK_MoodsInBetween = new SelectList(db.MoodsInBetweens, "PK", "FK_UserTable", mood.FK_MoodsInBetween);
+            return View(mood);
+        }
+
+        // GET: Moods/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Mood mood = db.Moods.Find(id);
+            if (mood == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.FK_MoodsInBetween = new SelectList(db.MoodsInBetweens, "PK", "FK_UserTable", mood.FK_MoodsInBetween);
+            return View(mood);
+        }
+
+        // POST: Moods/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PK,FK_MoodsInBetween,MoodNum,Date")] Mood mood)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(mood).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.FK_MoodsInBetween = new SelectList(db.MoodsInBetweens, "PK", "FK_UserTable", mood.FK_MoodsInBetween);
+            return View(mood);
+        }
+
+        // GET: Moods/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Mood mood = db.Moods.Find(id);
+            if (mood == null)
+            {
+                return HttpNotFound();
+            }
+            return View(mood);
+        }
+
+        // POST: Moods/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Mood mood = db.Moods.Find(id);
+            db.Moods.Remove(mood);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
