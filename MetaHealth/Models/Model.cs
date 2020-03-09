2f5785b1d2f300869ab8f011580d8b5a2942b4a8
HannaKristensen <hkristensen15@wou.edu>
2020-03-09 10:55:09 -0700

    Fixing some input for tasks

--- MetaHealth/Models/Model.cs
+++ MetaHealth/Models/Model.cs
@@ -15,7 +15,8 @@ namespace MetaHealth.Models
     public partial class Model : DbContext
     {
         public Model()
-            : base("AzureDB")
+        //   : base("AzureDB")
+        : base("DefaultConnection")
         {
         }
 
