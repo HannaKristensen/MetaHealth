f7337ffe4059c8beb1a3f16e087cd542f898ae98
abijohnson <aljohnson18@mail.wou.edu>
2020-04-02 12:38:05 -0700

    finished breathing space

---
+++ MetaHealth/Scripts/BreathingSpace.js
@@ -0,0 +1,30 @@
+﻿function heart() {
+
+    $("#shapeRow").fadeOut("slow")
+    $("#heartGif").fadeIn("slow")
+    $("#meditationRow").fadeIn("slow")
+
+}
+
+function star() {
+
+    $("#shapeRow").fadeOut("slow")
+    $("#starGif").fadeIn("slow") 
+    $("#meditationRow").fadeIn("slow")
+
+}
+
+function circle() {
+
+    $("#shapeRow").fadeOut("slow")
+    $("#circleGif").fadeIn("slow")
+    $("#meditationRow").fadeIn("slow")
+
+}
+
+function triangle() {
+    $("#shapeRow").fadeOut("slow")
+    $("#triGif").fadeIn("slow")
+    $("#meditationRow").fadeIn("slow")
+}
+
