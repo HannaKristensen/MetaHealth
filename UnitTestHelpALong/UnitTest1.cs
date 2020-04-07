f6cb2e36d346a7dbddf11d9825d865e01037bea5
HannaKristensen <hkristensen15@wou.edu>
2020-04-07 16:25:53 -0700

    Unit testing

--- UnitTestHelpALong/UnitTest1.cs
+++ UnitTestHelpALong/UnitTest1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Calendar.ASP.NET.MVC5.Controllers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTestHelpALong
@@ -6,9 +7,34 @@ namespace UnitTestHelpALong
     [TestClass]
     public class UnitTest1
     {
+        //Sorting out the needsAction Tasks. The Api returns all task and needs to be sorted
+        //on which ones are done or "needsAction"
         [TestMethod]
-        public void TestMethod1()
+        public void IsNotCompleteTaskSorted_TwoNeedsActionInList_Returns2AndPass()
         {
+            //Arrange objects, creating and setting them up as necessary.
+            CalendarController controller = new CalendarController();
+            string[] tasks = { "needsAction", "completed", "needsAction", "completed" };
+
+            //Act on an object.
+            string[] result = controller.CountingTasks(tasks);
+
+            //Assert that something is as expected.
+            Assert.AreEqual(2, result.Length);
+        }
+
+        [TestMethod]
+        public void IsNotCompleteTaskSorted_ZeroNeedsActionInList_ReturnsZeroAndPass()
+        {
+            //Arrange objects, creating and setting them up as necessary.
+            CalendarController controller = new CalendarController();
+            string[] tasks = { "completed", "completed", "completed", "completed" };
+
+            //Act on an object.
+            string[] result = controller.CountingTasks(tasks);
+
+            //Assert that something is as expected.
+            Assert.AreEqual(0, result.Length);
         }
     }
 }
