c377827b41f036c1373e7093a235dd4708bfd674
abijohnson <aljohnson18@mail.wou.edu>
2020-04-07 11:47:15 -0700

    Partially finished with user home page

--- MetaHealth/Scripts/TasksAPI.js
+++ MetaHealth/Scripts/TasksAPI.js
@@ -75,3 +75,37 @@ function showRedirect() {
 function hideContent() {
     document.getElementById("eventsPage").style.display = 'none';
 }
+
+function showAddTask() {
+
+    var addTask = document.getElementById("addTaskRow")
+    var displaySetting = addTask.style.display;
+
+        if (displaySetting == 'block') {
+
+            addTask.style.display = 'none';
+
+        }
+
+        else {
+
+            addTask.style.display = 'block';
+        }
+}
+
+function showPreMadeTasks() {
+
+    var multiTask = document.getElementById("preMadeTasks")
+    var displaySetting = multiTask.style.display;
+
+    if (displaySetting == 'block') {
+
+        multiTask.style.display = 'none';
+
+    }
+
+    else {
+
+        multiTask.style.display = 'block';
+    }
+}
