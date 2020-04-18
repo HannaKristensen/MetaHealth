974a2a12291c58bf35485f50fbdecc982a0e76c2
Jasmit Singh <jsingh18@wou.edu>
2020-04-18 01:13:42 -0700

    changes

--- MetaHealth/Models/CalendarEventGroup.cs
+++ MetaHealth/Models/CalendarEventGroup.cs
@@ -1,3 +1,16 @@
+/*
+Copyright 2015 Google Inc
+Licensed under the Apache License, Version 2.0(the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
@@ -5,24 +18,15 @@ using Google.Apis.Calendar.v3.Data;
 
 namespace Calendar.ASP.NET.MVC5.Models
 {
-    /// <summary>
-    /// A labeled group of calendar events.
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-
-using Google.Apis.Calendar.v3.Data;
-
-    namespace Calendar.ASP.NET.MVC5.Models
+    public class CalendarEventGroup
     {
-        public class CalendarEventGroup
-        {
-            [Required]
-            public string GroupTitle { get; set; }
+        [Required]
+        public string GroupTitle { get; set; }
 
-            /// <summary>
-            /// Gets or sets a sequence of calendar events to show under the group title.
-            /// </summary>
-            [Required]
-            public IEnumerable<Event> Events { get; set; }
-        }
+        /// <summary>
+        /// Gets or sets a sequence of calendar events to show under the group title.
+        /// </summary>
+        [Required]
+        public IEnumerable<Event> Events { get; set; }
     }
+}
