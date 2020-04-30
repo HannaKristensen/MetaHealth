3961b2a650256c086c15d83e77d86484ba52cd59
abijohnson <aljohnson18@mail.wou.edu>
2020-04-30 09:47:38 -0700

    finished mood hubgit add . git add . git add . git add . git add . git add . git add . !

---
+++ MetaHealth/Scripts/moodhub.js
@@ -0,0 +1,23 @@
+﻿function filter() {
+    var e = document.getElementById("moodDates");
+    var selectedItem = e.options[e.selectedIndex].value;
+
+    if (selectedItem == "all") {
+        document.getElementById("allTimeResults").style.display = 'inline-table';
+        document.getElementById("todayResults").style.display = 'none';
+        document.getElementById("thisWeekResults").style.display = 'none';
+    }
+
+    else if (selectedItem == "today") {
+        document.getElementById("allTimeResults").style.display = 'none';
+        document.getElementById("todayResults").style.display = 'inline-table';
+        document.getElementById("thisWeekResults").style.display = 'none';
+    }
+
+    else if (selectedItem == "week") {
+        document.getElementById("allTimeResults").style.display = 'none';
+        document.getElementById("todayResults").style.display = 'none';
+        document.getElementById("thisWeekResults").style.display = 'inline-table';
+    }
+
+}
